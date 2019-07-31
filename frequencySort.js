30af22467eca311c71f38e65d402802d19bfd959
Wei Ji <monokuroai@gmail.com>
2019-07-31 11:07:16 -0400

    updated frequencySort solution

--- frequencySort.js
+++ frequencySort.js
@@ -1,6 +1,6 @@
 var frequencySort = function (s) {
   let sArray = s.split('');
-  let touple = []
+  let result = "";
   let freqObj = sArray.reduce((acc, curr) => {
     if (curr in acc) {
       acc[curr]++;
@@ -10,8 +10,22 @@ var frequencySort = function (s) {
     return acc;
   }, {})
 
-  let keys = Object.keys(freqObj);
-  let maxOccurence = Math.max(...Object.values(freqObj));
+  let keys = Object.keys(freqObj)
+  keys = keys.sort((a, b) => {
+    return freqObj[b] - freqObj[a]
+  })
 
+  let maxOccurence = Math.max(...Object.values(freqObj));
   let bucket = new Array(maxOccurence)
+
+  keys.forEach(ele => {
+    for (let i = 0; i < freqObj[ele]; i++) {
+      result += ele
+    }
+  })
+  return result;
 };
+
+string1 = 'cccAaa'
+
+frequencySort(string1)
