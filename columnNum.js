a97f5efcafe1d5ac31fac2cda8bf47b16111acbf
Wei Ji <monokuroai@gmail.com>
2019-08-28 09:51:56 -0400

    added column number problem and solution

---
+++ columnNum.js
@@ -0,0 +1,23 @@
+var titleToNumber = function (s) {
+  let dictionary = { A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7, H: 8, I: 9, J: 10, K: 11, L: 12, M: 13, N: 14, O: 15, P: 16, Q: 17, R: 18, S: 19, T: 20, U: 21, V: 22, W: 23, X: 24, Y: 25, Z: 26 };
+  let counter = 0;
+  let sum = 0;
+
+  for (let i = s.length - 1; i > -1; i--) {
+    let current = s[i];
+    if (counter === 0) {
+      sum += dictionary[current];
+    } else {
+      console.log(counter, dictionary[current])
+      sum += Math.pow(26, counter) * dictionary[current]
+    }
+    counter++;
+  }
+  console.log(sum);
+  return sum
+
+};
+
+titleToNumber('AAA')
+titleToNumber('AA')
+titleToNumber('A')
