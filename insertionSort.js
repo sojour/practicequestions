f313d434f17755efd74a9c63b27a7976f507561c
Wei Ji <monokuroai@gmail.com>
2019-09-11 14:26:08 -0400

    added insertionSort problem and solution

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++){
+		let j = i;
+		while (j>0 && array[j]<array[j-1]){
+			let temp = array[j-1];
+			array[j-1] = array[j];
+			array[j] = temp;
+
+			j-=1;
+		}
+	}
+	return array;
+}
