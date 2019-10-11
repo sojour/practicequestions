079621802907b7f652f0703136713b93b5cbafd5
Wei Ji <monokuroai@gmail.com>
2019-10-11 17:06:42 -0400

    addes singleCycle problem and solution

---
+++ singleCycle.js
@@ -0,0 +1,22 @@
+function hasSingleCycle(array) {
+  // Write your code here.
+  let currIdx = 0;
+  let numVisited = 0;
+
+  while (numVisited < array.length) {
+    if (numVisited > 0 && currIdx === 0) {
+      return false;
+    }
+
+    numVisited++;
+    currIdx = getCurrIdx(array, currIdx);
+  }
+
+  return currIdx === 0;
+}
+
+function getCurrIdx(array, currIdx) {
+  const jump = array[currIdx];
+  const nextIdx = (currIdx + jump) % array.length;
+  return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}
