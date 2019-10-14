3ff9b384ac546a00973fa48a0493a91950310f8b
Wei Ji <monokuroai@gmail.com>
2019-10-14 17:08:00 -0400

    added LCA problem and solution

---
+++ lowestCA.js
@@ -0,0 +1,20 @@
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root || root === p || root === q) {
+    return root
+  }
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+
+
+  if (!left) {
+    console.log('here')
+    return right;
+  }
+
+  if (!right) {
+    return left;
+  }
+
+  return root
+};
