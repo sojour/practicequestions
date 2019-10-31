a1c6ff2b08a2e205ca154afe7ffe8316c3f82415
Wei Ji <monokuroai@gmail.com>
2019-10-31 16:41:45 -0500

    updated lowestCA

--- lowestCA.js
+++ lowestCA.js
@@ -17,3 +17,41 @@ var lowestCommonAncestor = function (root, p, q) {
 
   return root
 };
+
+
+//with ancestor property
+
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+  // Write your code here.
+  let d1Depth = getDepth(descendantOne, topAncestor);
+  let d2Depth = getDepth(descendantTwo, topAncestor);
+
+
+  if (d1Depth > d2Depth) {
+    return backTrack(descendantOne, descendantTwo, d1Depth - d2Depth);
+  } else {
+    return backTrack(descendantTwo, descendantOne, d2Depth - d1Depth);
+  }
+}
+
+function getDepth(descendant, topAncestor) {
+  let depth = 0;
+  while (descendant != topAncestor) {
+    depth++;
+    descendant = descendant.ancestor;
+  }
+  return depth;
+}
+
+function backTrack(small, large, difference) {
+  while (difference > 0) {
+    small = small.ancestor;
+    difference--;
+  }
+
+  while (small != large) {
+    small = small.ancestor;
+    large = large.ancestor;
+  }
+  return small;
+}
