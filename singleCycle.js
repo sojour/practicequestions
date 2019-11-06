73a904c79439e447043bdb90f4ba3657f91f9ed8
Wei Ji <monokuroai@gmail.com>
2019-11-05 21:58:58 -0600

    updated singleCycle

--- singleCycle.js
+++ singleCycle.js
@@ -20,3 +20,23 @@ function getCurrIdx(array, currIdx) {
   const nextIdx = (currIdx + jump) % array.length;
   return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
 }
+
+
+var hasCycle = function (head) {
+  if (head == null || head.next == null) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow != fast) {
+    if (fast == null || fast.next == null) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
+
+};
