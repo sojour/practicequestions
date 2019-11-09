b2789a0f430fcd103cea6e18d6ef9e817efdb0b2
Wei Ji <monokuroai@gmail.com>
2019-11-09 11:17:06 -0500

    added searchInSortedMatrix problem and solution

---
+++ searchInSortedMatrix.js
@@ -0,0 +1,16 @@
+function searchInSortedMatrix(matrix, target) {
+  // Write your code here.
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] < target) {
+      row++;
+    } else if (matrix[row][col] > target) {
+      col--;
+    } else {
+      return [row, col]
+    }
+  }
+  return [-1, -1];
+}
