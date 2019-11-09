90146b09cd724210bd7e5d0989d7169f80b643d9
Wei Ji <monokuroai@gmail.com>
2019-11-09 11:57:47 -0500

    added oddEvenList problem and solution

---
+++ oddEvenList.js
@@ -0,0 +1,16 @@
+var oddEvenList = function (head) {
+  if (!head) return head;
+
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+
+  while (even && even.next) {
+    odd.next = odd.next.next;
+    even.next = even.next.next;
+    odd = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
