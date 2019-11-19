94f83b328febd3ef283ae3f91e4e0291507662de
Wei Ji <monokuroai@gmail.com>
2019-11-19 11:12:51 -0500

    added waterMaxArea problem and solution

---
+++ waterMaxArea.js
@@ -0,0 +1,16 @@
+var maxArea = function (height) {
+  let maxArea = 0;
+  let l = 0;
+  let r = height.length - 1;
+
+  while (l < r) {
+    maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+
+    if (height[l] < height[r]) {
+      l++;
+    } else {
+      r--;
+    }
+  }
+  return maxArea;
+};
